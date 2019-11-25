4d3d5b45c859172b3e899fa9e0e69abd680935df
daihyun <judaihyun1@gmail.com>
2019-11-25 18:32:45 +0900

    [AssetManagement] next phase -> https://webpack.js.org/guides/asset-management/#loading-fonts

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode:'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -13,6 +14,12 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
                 ],
             },
         ],
